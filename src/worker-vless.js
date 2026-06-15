41d9d20e6302ed6e3a7eb48569fe1cc72a6b68ac
xuefei zhang <372289150@qq.com>
2026-06-15 23:49:54 +0800

    update userid

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'ac065fd5-d8ac-49d7-a7e9-4c15dcdc6aed';
 
 let proxyIP = '';
 
